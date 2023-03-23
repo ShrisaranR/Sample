3fc3784aea969692d86254d7749d0051434f503a
ANORA\shrisaran <shrisaran@anorasolutions.com>
2023-03-23 10:48:18 +0530

    division added by SN

--- Code_For_Git.py
+++ Code_For_Git.py
@@ -1,16 +1,29 @@
 print("Hello ShriSaran")
 print("Hello jegan")
+
+
 def sub(a, b):
     return a-b
-#2nd time file edited by JN
-def add(A,B):
-    C=A+B
+
+
+# 2nd time file edited by JN
+def add(A, B):
+    C = A + B
     return C
-#multiplication by Shrisarn
-def mul(a,b):
+
+
+# multiplication by Shrisaran
+def mul(a, b):
     return a*b
-X=int(input("Enter The First elements:"))
-Y=int(input("Enter The First elements:"))
-print("The Added Value",add(X,Y))
-print("The subtraction Value",sub(X,Y))
-print("The multiplication Value",sub(X,Y))
+
+
+def div(a, b):
+    return a / b, a % b
+
+
+X = int(input("Enter The First elements:"))
+Y = int(input("Enter The second elements:"))
+print(f"The Added Value = {add(X,Y)}")
+print(f"The subtraction Value = {sub(X,Y)}")
+print(f"The multiplication Value = {mul(X,Y)}")
+print(f"The division Value: quotient = {div(X,Y)[0]} remainder = {div(X,Y)[1]}")
