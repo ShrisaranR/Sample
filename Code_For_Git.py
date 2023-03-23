ad65d03a64069374a7c92bedc06e358ba00a97c3
ANORA\shrisaran <shrisaran@anorasolutions.com>
2023-03-23 10:08:40 +0530

    multiplication added by SN

--- Code_For_Git.py
+++ Code_For_Git.py
@@ -6,7 +6,11 @@ def sub(a, b):
 def add(A,B):
     C=A+B
     return C
+#multiplication by Shrisarn
+def mul(a,b):
+    return a*b
 X=int(input("Enter The First elements:"))
 Y=int(input("Enter The First elements:"))
 print("The Added Value",add(X,Y))
-print("The Added Value",sub(X,Y))
+print("The subtraction Value",sub(X,Y))
+print("The multiplication Value",sub(X,Y))
