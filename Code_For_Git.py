a4b7139e108650e6d5695c7d1851d8f3c89b05fa
ANORA\jegan <jegan@anorasolutions.com>
2023-03-23 09:58:23 +0530

    3_JN

--- Code_For_Git.py
+++ Code_For_Git.py
@@ -1,5 +1,6 @@
 print("Hello ShriSaran")
 print("Hello jegan")
+<<<<<<< HEAD
 
 
 def sub(a, b):
@@ -8,3 +9,12 @@ def sub(a, b):
 
 if operation == "-":
     ans = sub(x, y)
+=======
+#2nd time file edited by JN
+def add(A,B):
+    C=A+B
+    return C
+X=int(input("Enter The First elements:"))
+Y=int(input("Enter The First elements:"))
+print("The Added Value",add(X,Y))
+>>>>>>> 78fc0b2 (this code is edite_2_JN)
